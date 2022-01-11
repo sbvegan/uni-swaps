5bcc13d53ad7f558c560ed65b2ac6784dfe8558e
soyboy <sbvegan@protonmail.com>
2022-01-10 22:19:18 -0800

    jumping through hoops to import contracts with brownie

---
+++ contracts/uniswap/v2/Router02.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@uniswap-v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+contract Router02 {
+    IUniswapV2Router02 router;
+}
