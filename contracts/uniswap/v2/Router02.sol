6cf7ebaa25f6711b5c24c20ee18cf36d57e1bf41
soyboy <sbvegan@protonmail.com>
2022-01-11 18:30:14 -0800

    nuking the brownie project

--- contracts/uniswap/v2/Router02.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@uniswap-v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-
-contract Router02 {
-    IUniswapV2Router02 router;
-}
--- contracts/uniswap/v3/SwapRouter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@uniswap-v3-periphery/contracts/interfaces/ISwapRouter.sol ";
-
-contract SwapRouter {
-    ISwapRouter swapRouter;
-}
