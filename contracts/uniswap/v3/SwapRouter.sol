09d8723feb1eda045aa2dac86951d8fb7f14d20f
soyboy <sbvegan@protonmail.com>
2022-01-11 18:26:45 -0800

    looks like uniswap is easier to interact with via JS or TS

---
+++ contracts/uniswap/v3/SwapRouter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@uniswap-v3-periphery/contracts/interfaces/ISwapRouter.sol ";
+
+contract SwapRouter {
+    ISwapRouter swapRouter;
+}
